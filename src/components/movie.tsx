e322219f83c2b04543d95b1bfdfd5837cddd439d
hazemxiii <101417956+hazemxiii@users.noreply.github.com>
2026-04-02 16:58:02 +0200

    ui/add/show-details-btn

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -4,12 +4,18 @@ import MovieModel from "../models/movie_model";
 const Movie = ({ movie }: { movie: MovieModel }) => {
   const nav = useNavigate();
   return (
-    <div className="movie-card" onClick={() => nav("/movie/" + movie.id)}>
+    <div className="movie-card">
       <h3>{movie.title}</h3>
       <p>
         Year: <span className="year">{movie.prodYear}</span>
       </p>
       <p>ID: {movie.id}</p>
+      <button
+        className="btn-primary btn"
+        onClick={() => nav("/movie/" + movie.id)}
+      >
+        Show details
+      </button>
     </div>
   );
 };
